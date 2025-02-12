95f92f817d6898cf65210e8dec99d0a10f002003
Dagostini01 <pedro01dagostini@gmail.com>
2025-02-12 17:25:53 -0300

    feat: cabecalho

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,47 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView, TextInput, TouchableOpacity } from "react-native";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { s } from "./styles";
 
 export default function Home() {
     return (
-        <View>
-            <Text>Ola</Text>
+        <View style={s.container}>
+            <SafeAreaView style={s.form}>
+                <TextInput style={s.input} placeholder='Adicione uma nova tarefa' />
+                <TouchableOpacity style={s.button}>
+                    <View>
+                        <Text style={s.buttonText}>+</Text>
+                    </View>
+                </TouchableOpacity>
+            </SafeAreaView>
+
+            <View style={s.info}>
+                <View style={s.description}>
+                    <TouchableOpacity style={s.button2}>
+                        <View>
+                            <Ionicons name="calendar" size={15} color="#ffffff" />
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                <View style={s.description}>
+                    <Text style={s.descriptionText}>Criadas</Text>
+                    <View style={s.descriptionNumber}>
+                        <Text>0</Text>
+                    </View>
+                </View>
+                <View style={s.description}>
+                    <Text style={s.descriptionText2}>Concluídas</Text>
+                    <View style={s.descriptionNumber}>
+                        <Text>0</Text>
+                    </View>
+                </View>
+                <View style={s.description}>
+                    <TouchableOpacity style={s.button2}>
+                        <View>
+                            <Ionicons name="filter" size={15} color="#ffffff" />
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
     )
 }
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,88 @@
+import { StyleSheet } from "react-native";
+
+export const s = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 24,
+  },
+  form: {
+    flexDirection: "row",
+  },
+  input: {
+    flex: 1,
+    height: 56,
+    marginRight: 7,
+    backgroundColor: "#262626",
+    borderRadius: 5,
+    color: "#FFF",
+    padding: 16,
+    fontSize: 16,
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: "#4EA8DE",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button2: {
+    width: 28,
+    height: 28,
+    borderRadius: 5,
+    backgroundColor: "#4EA8DE",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 24,
+  },
+  info: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 30,
+  },
+  description: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  descriptionText: {
+    fontSize: 15,
+    color: "#4EA8DE",
+    fontWeight: "bold",
+    marginRight: 5,
+  },
+  descriptionText2: {
+    fontSize: 15,
+    color: "#8284FA",
+    fontWeight: "bold",
+    marginRight: 5,
+  },
+  descriptionNumber: {
+    backgroundColor: "#33333333",
+    padding: 4,
+    borderRadius: 5,
+    width: 25,
+    alignItems: "center",
+  },
+  tarefaView: {},
+  listEmptyText1: {
+    color: "#33333333",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 44,
+  },
+  listEmptyText2: {
+    color: "#33333333",
+    textAlign: "center",
+    fontSize: 30,
+  },
+  containerlistEmptyText: {
+    marginTop: 100,
+    alignItems: "center",
+  },
+});
